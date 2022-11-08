244c46754273dcacb449c26c1e65e1e1366412b6
Vladimir <10219vladimircvetkovic@etsntesla.edu.rs>
2022-11-08 10:56:07 +0100

    Initial commit

---
+++ src/main/java/com/example/igrica/HelloController.java
@@ -0,0 +1,30 @@
+package com.example.igrica;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    @FXML
+    public void onHelloButtonClick(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("scene1view.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
